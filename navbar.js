922cffbf65af803c57f0b9c78592ec99b85fef2e
Johnny Nguyen <55181277+njohnny92@users.noreply.github.com>
2021-09-22 00:52:20 -0700

    added packges

--- navbar.js
+++ navbar.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
 const navbar = () => {
     
 }
